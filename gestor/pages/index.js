8b8b6853779e47b091995f197ae8732cc7be6f92
laurods <itrafor@gmail.com>
2020-12-22 17:25:59 -0200

    Atualizacao 22122020l

--- gestor/pages/index.js
+++ gestor/pages/index.js
@@ -216,6 +216,10 @@ class Home extends Component {
         <Head>
           <title>Create Next App</title>
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
         </Head>
         <main>
           <h1 className="title">
