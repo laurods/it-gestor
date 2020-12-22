355c643ec9676fff9d8d06ca05c2029a6acec574
laurods <itrafor@gmail.com>
2020-12-22 17:29:16 -0200

    Atualizacao 22122020m

--- gestor/pages/products.js
+++ gestor/pages/products.js
@@ -1,19 +1,9 @@
 import GlobalStyle from '../styles/global';
 import { connectToDatabase } from '../util/mongodb';
-import Button from '@material-ui/core/Button';
 
 export default function Products({ products }) {
   return (
-    <div>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-      </Head>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+    <div>      
       <div className="container">
         <main>
           <div className="card">
