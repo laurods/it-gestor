9c429c71f869e27552b9d57dd9caeb3c806dcdbb
laurods <itrafor@gmail.com>
2020-12-23 15:42:00 -0200

    Atualizacao 23122020p

--- gestor/components/screen/Layout/tableRowProduct.js
+++ gestor/components/screen/Layout/tableRowProduct.js
@@ -6,14 +6,11 @@ import IconButton from '@material-ui/core/IconButton';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import { connectToDatabase } from '../../util/mongodb';
-import SearchBar from '../../components/screen/Layout/searchbar'
+
 
 
 const useRowStyles = makeStyles({
