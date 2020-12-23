4994835450c1a5b5e9a9631c633d15914777d3ca
laurods <itrafor@gmail.com>
2020-12-22 22:46:16 -0200

    Atualizaçao 22022020ae

--- gestor/pages/product/index.js
+++ gestor/pages/product/index.js
@@ -13,7 +13,7 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import { connectToDatabase } from '../util/mongodb';
+import { connectToDatabase } from '../../util/mongodb';
 
 const useRowStyles = makeStyles({
   root: {
