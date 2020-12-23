f74ccebb8166007d3088d63ffedd13ac6ef38ecc
laurods <itrafor@gmail.com>
2020-12-23 15:39:32 -0200

    Atualizacao 23122020n

---
+++ gestor/components/screen/Layout/tableRowProduct.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { connectToDatabase } from '../../util/mongodb';
+import SearchBar from '../../components/screen/Layout/searchbar'
+
+
+const useRowStyles = makeStyles({
+  root: {
+    '& > *': {
+      borderBottom: 'unset',
+          
+    },
+  },
+  
+});
+
+
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+
+  return (    
+    <React.Fragment> 
+            <TableRow className={classes.root}>
+                <TableCell>
+                <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+                    {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                {row.xprod}
+                </TableCell>
+                <TableCell align="right">{row.custoUn}</TableCell>        
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <Box margin={1}>             
+                    <Table size="small" aria-label="purchases">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>Date</TableCell>
+                            <TableCell>Customer</TableCell>
+                            <TableCell align="right">Amount</TableCell>
+                            <TableCell align="right">Total price ($)</TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        
+                            <TableRow key={row.id}>
+                            <TableCell component="th" scope="row">
+                                {row.qcom}
+                            </TableCell>
+                            <TableCell>{row.picms}</TableCell>
+                            <TableCell align="right">{row.perDifcaIcms}</TableCell>
+                            <TableCell align="right">
+                                {row.vprod}
+                            </TableCell>
+                            </TableRow>
+                        
+                        </TableBody>
+                    </Table>
+                    </Box>
+                </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+  );
+}
