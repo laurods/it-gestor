d0e4fcc37a310508a2eb3cefe85614ec21c43bc8
laurods <itrafor@gmail.com>
2020-12-23 15:58:24 -0200

    Atualizacao 23122020r

--- gestor/pages/product/all.js
+++ gestor/pages/product/all.js
@@ -6,6 +6,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import { connectToDatabase } from '../../util/mongodb';
 import SearchBar from '../../components/screen/Layout/searchbar';
 import TableRowProduct from '../../components/screen/Layout/tableRowProduct';
@@ -13,23 +14,31 @@ import TableRowProduct from '../../components/screen/Layout/tableRowProduct';
 export default function CollapsibleTable({ products }) {
   return (
     <div>
-        <SearchBar />
-        <TableContainer component={Paper}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Descrição</TableCell>
-            <TableCell align="right">Custo Un</TableCell>         
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((row) => (
-            <TableRowProduct key={row.id} row={row} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+            <Paper>
+            <SearchBar />
+            </Paper>
+            </Grid>
+            <Grid item xs={6}>
+            <TableContainer component={Paper}>
+                <Table aria-label="collapsible table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell />
+                        <TableCell>Descrição</TableCell>
+                        <TableCell align="right">Custo Un</TableCell>         
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {products.map((row) => (
+                        <TableRowProduct key={row.id} row={row} />
+                    ))}
+                    </TableBody>
+                </Table>
+                </TableContainer>
+            </Grid>        
+        </Grid>
     </div>
     
   );
