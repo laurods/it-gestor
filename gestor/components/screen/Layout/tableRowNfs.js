43f5837a6049cb3afeb91549fb68a38ea6f2c76b
laurods <itrafor@gmail.com>
2020-12-28 16:05:09 -0200

    atualizaçao 28122020d

--- gestor/components/screen/Layout/tableRowNfs.js
+++ gestor/components/screen/Layout/tableRowNfs.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
@@ -35,32 +30,8 @@ export default function Row(props) {
                     {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
                 </IconButton>
                 </TableCell>
-                <TableCell component="th" scope="row">{row.xprod}</TableCell>
-                <TableCell align="right">{row.custoUn}</TableCell>
-                <TableCell align="right">{row.ceantrib}</TableCell>        
-            </TableRow>
-            <TableRow>
-                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                    <Box margin={1}>             
-                    <Table size="small" aria-label="purchases">
-                        <TableHead>
-                        <TableRow>
-                            <TableCell>NF</TableCell>  
-                        </TableRow>
-                        </TableHead>
-                        <TableBody>
-                        
-                            <TableRow key={row.id}>
-                            <TableCell component="th" scope="row">{row.nnf}</TableCell>                            
-                            </TableRow>
-                        
-                        </TableBody>
-                    </Table>
-                    </Box>
-                </Collapse>
-                </TableCell>
-            </TableRow>
+                <TableCell component="th" scope="row">{row.nnf}</TableCell>                        
+            </TableRow>           
         </React.Fragment>
   );
 }
