abd80feafeccefd4bc28ce2468588a8bcb929742
laurods <itrafor@gmail.com>
2020-12-31 18:08:45 -0200

    atualizaçao 31122020ab

--- gestor/pages/api/user.js
+++ gestor/pages/api/user.js
@@ -10,5 +10,18 @@ export default async (req, res) => {
     } catch {
       print(e);
     }
+  }else if (req.method === 'GET') {
+    try {
+      const email = req.query;
+      const { db } = await connectToDatabase();
+      const response = await db.collection('users').findOne({email:email});
+      res.status(200).json(response.ops[0]);
+    } catch {
+      print(e);
+    }
+  } else {
+    //  block of code to be executed if the condition1 is false and condition2 is false
   }
+
+
 };
