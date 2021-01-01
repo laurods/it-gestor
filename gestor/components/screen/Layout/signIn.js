a9d1a1a82938e1dd4500378738668088d3f18735
laurods <itrafor@gmail.com>
2021-01-01 10:57:07 -0200

    atualizaçao 0101202af

--- gestor/components/screen/Layout/signIn.js
+++ gestor/components/screen/Layout/signIn.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles(() => ({
 export default function SignIn() {
   const classes = useStyles();
   const getCnpj = () => {
-    cnpj = document.getElementById('login-email').value;
+    const cnpj = document.getElementById('login-email').value;
     console.log(cnpf);
   }
 
