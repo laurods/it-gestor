4a63a90e131e141761f74826ec5c5e7fa51b481c
laurods <itrafor@gmail.com>
2021-01-01 10:03:04 -0200

    atualizaçao 01012021f

--- gestor/pages/api/user/updateOne.js
+++ gestor/pages/api/user/updateOne.js
@@ -5,11 +5,11 @@ export default async (req, res) => {
     try {
       const user = req.body;
       const email = user.email;
-      const cnpjEmitente = user.cnpjEmitente;
+      const cnpjDestinatario = user.cnpjDestinatario;
       const { db } = await connectToDatabase();
       const response = await db.collection('users').updateOne(
-        { "email" : {email} },
-        { $set: {"email" : {email}, "cnpjEmitente" : {cnpjEmitente} } },
+        { "email" : email },
+        { $set: {"email" : email, "cnpjEmitente" : cnpjDestinatario } },
         { upsert: true }
       );
       res.status(200).json(response);
