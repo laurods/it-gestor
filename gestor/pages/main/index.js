f382b473856f1cbb90427b0427c03765dd0ba26e
laurods <itrafor@gmail.com>
2021-01-01 17:13:05 -0200

    atualizaçao 0101202ar

--- gestor/pages/main/index.js
+++ gestor/pages/main/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useRouter } from 'next/router'
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 
-export default function About() {
+export default function Main() {
+  const router = useRouter();
+  console.log(router.query)
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
