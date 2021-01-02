b6c18417fd0298cd65bfe588e008c6acc4699c61
laurods <itrafor@gmail.com>
2021-01-02 10:39:01 -0200

    atualizaçao 02012021i

--- gestor/pages/main/[cnpj].js
+++ gestor/pages/main/[cnpj].js
@@ -101,7 +101,7 @@ export async function getServerSideProps() {
   
   const { db } = await connectToDatabase();
   
-  const products = await db.collection('products').find({'cnpjDestinatario': '04921325000168'}).toArray();
+  const products = await db.collection('products').find({'cnpjDestinatario': router.query.cnpj}).toArray();
 
   return {
     props: {
