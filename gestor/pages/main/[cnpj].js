5506dbcb8a80ec68e5c9f45732c076ad3be4156b
laurods <itrafor@gmail.com>
2021-01-02 11:43:42 -0200

    atualizaçao 02012021q

--- gestor/pages/main/[cnpj].js
+++ gestor/pages/main/[cnpj].js
@@ -98,9 +98,7 @@ export default function CenteredGrid({ products }) {
 }
 
 export async function getServerSideProps() {
-  const router = useRouter();
-  const { cnpj } = router.query;
-   
+  
   const { db } = await connectToDatabase();
   
   const products = await db.collection('products').find({'cnpjDestinatario': '04921325000168'}).toArray();
