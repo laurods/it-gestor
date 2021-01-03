4b54dee0f40ede6e518bf262c69e0059d9782334
laurods <itrafor@gmail.com>
2021-01-03 12:17:31 -0200

    atualizaçao 03012021ai

--- gestor/pages/api/user/login.js
+++ gestor/pages/api/user/login.js
@@ -1,6 +1,7 @@
 import { connectToDatabase } from '../../../util/mongodb';
 import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
+import cookie from 'cookie';
 
 export default async (req, res) => {
   if (req.method === 'POST') {
@@ -15,8 +16,14 @@ export default async (req, res) => {
             if(!err && result){
                 const claims = {sub: response[0]._id, userEmail: response[0].email}
                 const jwt = sign(claims, '4a56384b-61de-4446-bcec-49515bb71a0f', { expiresIn: '1h' });
-
-                res.json({authTokeb: jwt});
+                res.setHeader('Set-Cookie', cookie.serialize('auth', jwt, {
+                  httpOnly: true,
+                  secure: true,
+                  sameSite: 'strict',
+                  maxAge: 3600,
+                  path: '/'
+                }))
+                res.json({message: 'Welcome back to the app!'});
             }else{
                 res.json({message: 'ups, something went wrong!'});
             }
