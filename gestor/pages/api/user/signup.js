398c731af0e3cb6d8662c65e7355700839ac2753
laurods <itrafor@gmail.com>
2021-01-03 06:41:21 -0200

    atualizaçao 03012021c

--- gestor/pages/api/user/signup.js
+++ gestor/pages/api/user/signup.js
@@ -1,15 +1,25 @@
 import { connectToDatabase } from '../../../util/mongodb';
+import { hash } from 'bcrypt';
+import { has } from 'lodash';
 
 export default async (req, res) => {
   if (req.method === 'POST') {
+    const data = req.body;
+    let password = data.password;
+    hash(password, 10, async function(err, hash) {
+      // Store hash in your password DB.
     try {       
-      const user = req.body;
+      const user = {
+        name: data.name,
+        password: hash
+      };
       const { db } = await connectToDatabase();
       const response = await db.collection('users').insertOne(user);
-      res.status(200).json(response);      
+      res.status(200).json(response.ops[0]);      
     } catch {
       print(e);
     }
+  });
   } else {
     //  block of code to be executed if the condition1 is false and condition2 is false
   }
