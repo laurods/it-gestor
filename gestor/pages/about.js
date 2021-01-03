7ad7a6ffa6bf00326451971e2449d034e4d5e1d5
laurods <itrafor@gmail.com>
2021-01-03 13:15:19 -0200

    atualizaçao 03012021al

--- gestor/pages/about.js
+++ gestor/pages/about.js
@@ -3,12 +3,14 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import { connectToDatabase } from '../util/mongodb';
 
 
-export default function About() { 
+export default function About({users}) { 
  
   return (
     <Container maxWidth="sm">
+      {users}
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
@@ -20,3 +22,16 @@ export default function About() {
     </Container>
   );
 };
+
+export async function getServerSideProps() {
+  
+  const { db } = await connectToDatabase();
+  const users = await db.collection('users').find().toArray();
+
+  return {
+    props: {
+      users: JSON.parse(JSON.stringify(users)),
+    },
+  };
+  
+}
