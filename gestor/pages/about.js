012f839871887a55455089b1f825300892b6f4ae
laurods <itrafor@gmail.com>
2021-01-03 09:41:35 -0200

    atualizaçao 03012021ah

--- gestor/pages/about.js
+++ gestor/pages/about.js
@@ -3,19 +3,9 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import { verify } from 'jsonwebtoken';
 
-export const authenticated = fn => async (req, res) => {
-  verify(req.headers.authorization, '4a56384b-61de-4446-bcec-49515bb71a0f', async function(err, decoded) {
-    if(!err && decoded){
-      return await fn(req, res);
-    }
 
-    res.status(500).json({message: 'Sorry you are not authenticated'});
-  });
-}
-
-export default authenticated(function About() { 
+export default function About() { 
  
   return (
     <Container maxWidth="sm">
@@ -29,4 +19,4 @@ export default authenticated(function About() {
       </Box>
     </Container>
   );
-});
+};
