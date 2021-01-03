428e6c6ed7695f0ce844790b9cf8a87937653192
laurods <itrafor@gmail.com>
2021-01-03 06:06:20 -0200

    atualizaçao 03012021a

---
+++ gestor/pages/api/user/singnup.js
@@ -0,0 +1,18 @@
+import { connectToDatabase } from '../../../util/mongodb';
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    try {       
+      const user = req.body;
+      const { db } = await connectToDatabase();
+      const response = await db.collection('users').insertOne(user);
+      res.status(200).json(response.ops[0]);      
+    } catch {
+      print(e);
+    }
+  } else {
+    //  block of code to be executed if the condition1 is false and condition2 is false
+  }
+
+
+};
