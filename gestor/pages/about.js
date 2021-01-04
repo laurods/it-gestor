67ce6fc8afcd1bc5bda98dc31992a7f70a03232e
laurods <itrafor@gmail.com>
2021-01-04 07:12:22 -0200

    atualizaçao 04012021e

--- gestor/pages/about.js
+++ gestor/pages/about.js
@@ -2,16 +2,15 @@ import React from 'react';
 import {NextPageContext} from 'next';
 import { myGet } from '../pages/api/myGet';
 
-export default function User() {
+export default function User({people}) {
 
   return <div>Hello People</div>  
       
 
 }
-/*
-User.getInitialProps = async (NextPageContext) => {
+
+User.getInitialProps = async (ctx) => {
   
-  const json = await myGet('https://it-gestor.vercel.app/api/user/findAll', NextPageContext);
+  const json = await myGet('https://it-gestor.vercel.app/api/user/findAll', ctx);
   return{people: json};
 }
-*/
