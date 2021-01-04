7be1ca7e92ee8203d3732b952016f0949c090521
laurods <itrafor@gmail.com>
2021-01-04 07:30:31 -0200

    atualizaçao 04012021i

--- gestor/pages/about.js
+++ gestor/pages/about.js
@@ -4,13 +4,18 @@ import { myGet } from '../pages/api/myGet';
 
 export default function User({people}) {
 
-  return <div>Hello People</div>  
+  return <div>Hello People{JSON.stringify(people)}</div>  
       
 
 }
 
 User.getInitialProps = async (ctx) => {
-  const resp = await fetch('https://it-gestor.vercel.app/api/user/findAll');
+  const cookie = ctx.req.headers.cookie;
+  const resp = await fetch('https://it-gestor.vercel.app/api/user/findAll', {
+    headers: {
+      cookie: cookie
+    }
+  });
   const json = await resp.json();
   return{people: json};
 }
