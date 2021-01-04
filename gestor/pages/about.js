baa14c30394081d0266d147db303a3dad31aba79
laurods <itrafor@gmail.com>
2021-01-04 07:50:53 -0200

    atualizaçao 04012021j

--- gestor/pages/about.js
+++ gestor/pages/about.js
@@ -3,13 +3,13 @@ import {NextPageContext} from 'next';
 import { myGet } from '../pages/api/myGet';
 
 export default function User({people}) {
-
-  return <div>Hello People{JSON.stringify(people)}</div>  
+  console.log(people);
+  return <div>Hello People</div>  
       
 
 }
-
-User.getInitialProps = async (ctx) => {
+/*
+User.getInitialProps = async ({ctx}) => {
   const cookie = ctx.req.headers.cookie;
   const resp = await fetch('https://it-gestor.vercel.app/api/user/findAll', {
     headers: {
@@ -18,4 +18,19 @@ User.getInitialProps = async (ctx) => {
   });
   const json = await resp.json();
   return{people: json};
+}
+*/
+
+export async function getServerSideProps(context) {
+  const cookie = context.req.headers.cookie;
+  const people = await fetch('https://it-gestor.vercel.app/api/user/findAll', {
+    headers: {
+      cookie: cookie
+    }
+  });  
+  return {
+    props: {
+      people: JSON.parse(JSON.stringify(people)),
+    }, // will be passed to the page component as props
+  }
 }
