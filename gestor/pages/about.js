82a13c870261c8f01d818e600831be14724b5574
laurods <itrafor@gmail.com>
2021-01-05 16:09:11 -0200

    atualizaçao 05012021z

--- gestor/pages/about.js
+++ gestor/pages/about.js
@@ -9,29 +9,13 @@ export default function User({people}) {
 
   return (
     <div>
-       <p>About</p>
-     
-
-   
-   
+      <p>About</p>
     </div>  
   
     )  
 
 }
-/*
-User.getInitialProps = async ({ctx}) => {
-  const cookie = ctx.req.headers.cookie;
-  const resp = await fetch('https://it-gestor.vercel.app/api/user/findAll', {
-    headers: {
-      cookie: cookie
-    }
-  });
-  const json = await resp.json();
-  return{people: json};
-}
 
-*/
 export async function getServerSideProps(context) {
   const cookie = context.req.headers.cookie;
   if(cookie !=='undefined'){
@@ -61,23 +45,6 @@ export async function getServerSideProps(context) {
   };
 
   }
-/*
-  if(resp.status === 502){
-   
-    return {};
-  }
 
-  if(resp.status === 401 && !context.req){
-    Router.replace('/login');
-    return {};
-  }
-
-  if(resp.status === 401 && context.req){
-    context.res.writeHead(302, {
-      Location: 'https://it-gestor.vercel.app/login'
-    });
-    context.res.end();
-    return;
-  }
-  */
 }
+
