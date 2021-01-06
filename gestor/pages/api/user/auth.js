079e92135135dd80458173047c0a3382b271ae57
laurods <itrafor@gmail.com>
2021-01-06 10:12:29 -0200

    atualizaçao 06012021l

--- gestor/pages/api/user/auth.js
+++ gestor/pages/api/user/auth.js
@@ -34,7 +34,7 @@ export default authenticated(async function getUser (req, res) {
   if (req.method === 'GET') {
     try {             
       const { db } = await connectToDatabase();
-      const response = await db.collection('users').find().toArray();
+      const response = await db.collection('users').find({'email':req.userEmail}).toArray();
       res.status(200).json({response: response, email: req.userEmail});      
     } catch {
       print(e);
