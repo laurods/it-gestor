f451a44c0efd51ecc32a01f64cab022c40370e65
laurods <itrafor@gmail.com>
2021-01-10 10:15:10 -0200

    atualizacao 10012021m

--- gestor/components/Main/headerMain.js
+++ gestor/components/Main/headerMain.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
-export default function HeaderMain() {
-    
+export default function HeaderMain(props) {
+    const { email } = this.props;
+    console.log(email);
     return (
       <div>
      <Grid container spacing={3}>
--- gestor/pages/main/index.js
+++ gestor/pages/main/index.js
@@ -11,7 +11,7 @@ export default function Index({profile}) {
     <div>
         <CssBaseline />
         <SearchAppBar />
-        <HeaderMain />
+        <HeaderMain email={profile.email}/>
 
        
        
