f2b17265c4cce57d41504024b3d3d7eb68ea993c
laurods <itrafor@gmail.com>
2021-01-10 09:12:38 -0200

    atualizacao 10012021b

--- gestor/pages/main/index.js
+++ gestor/pages/main/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import jwt from 'jsonwebtoken';
-export default function Blog({profile}) {
+export default function Index({profile}) {
     console.log(profile); 
 
   return (
     <div>
-      <p>About{profile[0].email}</p>
+      <p>About</p>
     </div>  
   
     )  
@@ -42,39 +41,6 @@ export async function getServerSideProps(context) {
     };
   
     }
-    
- /*   
- const { req } = context; 
- const { token } = getAppCookies(req);
- const profile = token ? verifyToken(token.split(' ')[1]) : '';
- return {
-    props: {      
-      profile,
-    },
-  };
-
-
-  function getAppCookies(req) {
-    const parsedItems = {};
-    if (req.headers.cookie) {
-      const cookiesItems = req.headers.cookie.split('; ');
-      cookiesItems.forEach(cookies => {
-        const parsedItem = cookies.split('=');
-        parsedItems[parsedItem[0]] = decodeURI(parsedItem[1]);
-      });
-    }
-    return parsedItems;
-  }
-
-}
-
-function verifyToken(jwtToken) {
-  try {
-    return jwt.verify(jwtToken, '4a56384b-61de-4446-bcec-49515bb71a0f');
-  } catch (e) {
-    console.log('e:', e);
-    return null;
-  }
-*/  
+     
 }
 
