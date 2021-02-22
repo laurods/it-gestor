16bd621d836dd7ea9a84e05c7641deede6e00785
laurods <itrafor@gmail.com>
2021-02-21 21:35:15 -0300

    login context_api 21022021a

--- gestor/pages/api/user/authDrinkeat.js
+++ gestor/pages/api/user/authDrinkeat.js
@@ -4,6 +4,13 @@ import { sign } from 'jsonwebtoken';
 import cookie from 'cookie';
 
 export default async (req, res) => { 
+  // Allow Origins
+  res.header("Access-Control-Allow-Origin", "*");
+  // Allow Methods
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  // Allow Headers
+  res.header("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization");
+  // Handle preflight, it must return 200
   if (req.method === 'POST') {
     const user = req.body.user
     const email = user.email;
