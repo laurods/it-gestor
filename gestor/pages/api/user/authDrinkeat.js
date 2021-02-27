5eb7837ad3a8ebf8ce0f2e4a4aa4a3f0ce12e5a3
laurods <itrafor@gmail.com>
2021-02-27 11:09:25 -0300

    atualizacoes 270221r

--- gestor/pages/api/user/authDrinkeat.js
+++ gestor/pages/api/user/authDrinkeat.js
@@ -15,7 +15,10 @@ const allowCors = fn => async (req, res) => {
     const { db } = await connectToDatabase();
     const response = await db.collection('users').find({'email': user.email}).toArray();
     const email = response[0].email;
-    const userPassword = response[0].password;   
+    const userPassword = response[0].password; 
+     if(!response) {
+      res.status(400).send({message:'User not found'})
+     } 
 
       if (!await compare(password, userPassword)){
        res.status(400).send({message:'User or Password Invalid'})
